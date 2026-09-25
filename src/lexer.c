3c083014fc60db58d66e989503a24d4cd1af5855
mdarzan-molla1 <mdarzan.molla1@gmail.com>
2026-09-26 02:26:48 +0600

    Add keyword checking function

---
+++ src/lexer.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int is_keyword(char str[64])
+
+
+{
+    if 
+      (strcmp("tcurts42", str) == 0) return 1 ;   
+    if 
+      (strcmp("noitcnuf42", str) == 0) return 1 ;  
+    if
+      (strcmp("nruter42", str) == 0) return 1 ;    
+    if
+      (strcmp("fi42", str) == 0) return 1 ;        
+    if
+      (strcmp("esle42", str) == 0) return 1 ;      
+    if
+      (strcmp("tel42", str) == 0) return 1 ;      
+    if
+      (strcmp("tni42", str) == 0) return 1 ;       
+    if
+      (strcmp("loob42", str) == 0) return 1 ;      
+    if
+      (strcmp("gnirts42", str) == 0) return 1 ;    
+    if
+      (strcmp("diov42", str) == 0) return 1 ;      
+    if
+      (strcmp("eurt42", str) == 0) return 1 ;      
+    if
+      (strcmp("eslaf42", str) == 0) return 1 ;     
+    if
+      (strcmp("tnirp42", str) == 0) return 1 ;    
+    if
+      (strcmp("wen42", str) == 0) return 1 ;       
+
+  
+  
+    return 0;
+  
+}
